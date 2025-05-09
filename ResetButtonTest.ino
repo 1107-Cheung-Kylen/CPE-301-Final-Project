2de2bbda24c4fb692a5df442dc22505eec4352a2
Kylen Cheung <144738661+1107-Cheung-Kylen@users.noreply.github.com>
2025-05-08 20:26:44 -0700

    Change button to A15

--- ResetButtonTest.ino
+++ ResetButtonTest.ino
@@ -1,21 +1,22 @@
 // Authors: Kylen Cheung and Jonathan Summers
 
-// Button - PG0
-volatile unsigned char* port_g = (unsigned char*) 0x34; 
-volatile unsigned char* ddr_g  = (unsigned char*) 0x33; 
-volatile unsigned char* pin_g  = (unsigned char*) 0x32; 
+// Button - A15 (PK7)
+// Opens analog for ports Port K0-7
+volatile unsigned char* port_k = (unsigned char*) 0x108; 
+volatile unsigned char* ddr_k  = (unsigned char*) 0x107; 
+volatile unsigned char* pin_k  = (unsigned char*) 0x106; 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   // set PK2 to input
-  *ddr_g &= 0b00000001;  // 
+  *ddr_k &= 0b10000000;  // 
   // set PD0 to output
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(*pin_g & 0b00000001){ // if high;
+  if(*pin_k & 0b10000000){ // if high;
     Serial.println("High");
   }
   else{ // if low
